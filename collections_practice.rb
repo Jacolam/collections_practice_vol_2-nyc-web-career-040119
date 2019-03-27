f1d21eaa8be655e62ef1fc34d89e9bdce8be3d0c
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-27 16:12:40 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -101,7 +101,7 @@ def organize_schools(array)
   sorted_schools = {}
   array.each do |school,city|
    binding.pry
-    if sorted_schools.key?()
+    if sorted_schools.key?(city[:location])
   end 
   
 end 
